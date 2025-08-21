34c3f7c96041b3b1fd33866af98ba9a48c9092c8
Silous <silous666@gmail.com>
2025-08-21 11:38:16 +0200

    add template

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RawTextCheck-parsers
 Community parsers for RawTextCheck – extend support for custom file formats.
+
+For examples of parser implementations, see the [default parsers included in RawTextCheck](https://github.com/Silous888/RawTextCheck/tree/master/rawtextcheck/default_parser).
---
+++ template_parser.py
@@ -0,0 +1,40 @@
+# == Imports ==================================================================
+
+from logging import Logger
+
+from rawtextcheck.logger import get_logger
+from rawtextcheck.newtype import ParserArgument
+
+
+# == Constants ================================================================
+
+# examples of argume
+# ARG_1 = ParserArgument(name="arg1", optional=False)
+# ARG_2 = ParserArgument(name="arg2", optional=True)
+
+# Fill the list with arguments (ARG_1, ARG_2...) you have.
+LIST_ARGUMENTS: list[ParserArgument] = []
+
+
+# == Global Variables =========================================================
+
+logger: Logger = get_logger(__name__)
+# You can use it like this:
+# logger.error("Error when parsing the file %s : %s", filepath, e)
+# logger.error, logger.warning, logger.info
+
+
+# == Functions ================================================================
+
+def parse_file(filepath: str, arguments: dict[str, str]) -> list[tuple[str, str]]:
+    """Parse a file and return each non-empty line with identifier (line number
+    or something else) 
+
+    Args:
+        filepath (str): Path to the file
+        arguments (dict[str, str]): arguments of the parser
+
+    Returns:
+        list[tuple[str, str]]: List of (id, line).
+    """
+    return []
