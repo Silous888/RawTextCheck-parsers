563fd0c580a8abba886795c3a5c7e6938233122f
Silous <silous666@gmail.com>
2025-08-22 20:06:27 +0200

    update template with is_filepath_valid and get_filename

--- template_parser.py
+++ template_parser.py
@@ -1,3 +1,5 @@
+"""Template parser for RawTextCheck."""
+
 # == Imports ==================================================================
 
 from logging import Logger
@@ -26,9 +28,41 @@ logger: Logger = get_logger(__name__)
 
 # == Functions ================================================================
 
+
+# Implement is_filepath_valid only if needed. If your file is an offline file,
+# you probably don't need this function. Used to check if the file can be parsed
+# by this parser and if the the file exists.
+
+# def is_filepath_valid(filepath: str) -> bool:
+#     """check if the filepath is a valid file for this parser
+
+#     Args:
+#         filepath (str): path of the file
+
+#     Returns:
+#         bool: True if valid, False otherwise
+#     """
+#     return False
+
+
+# implement get_filename only if needed. If your file is an offline file, you probably don't need
+# this function. Used for for saving the results.
+
+# def get_filename(filepath: str) -> str:
+#     """get the filename of the filepath
+
+#     Args:
+#         filepath (str): path of the file
+
+#     Returns:
+#         str: name of the file
+#     """
+#     return filepath
+
+
 def parse_file(filepath: str, arguments: dict[str, str]) -> list[tuple[str, str]]:
     """Parse a file and return each non-empty line with identifier (line number
-    or something else) 
+    or something else)
 
     Args:
         filepath (str): Path to the file
